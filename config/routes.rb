0f65a6a24d3a991f7fff0365ec57d82520ff2cf2
Erick Brower <cerickbrower@gmail.com>
2013-03-10 19:52:41 -0400

    Installed Zurb Foundation 4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dex::Application.routes.draw do
+  root :to => 'main#index'
+  resources :entries
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +14,6 @@ Dex::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  resources :entries
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +50,6 @@ Dex::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
