510e27b82120fc7766e8deeeb170eb1849d1b19d
Erick Brower <cerickbrower@gmail.com>
2013-03-10 23:49:50 -0400

    Added markdown rendering of Entry content with Redcarpet.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.12'
 gem 'bcrypt-ruby'
 gem 'pg'
+gem 'redcarpet'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,8 @@
 module EntriesHelper
+  def markdown_render(content)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
+                                       :autolink => true, 
+                                       :space_after_headers => true)
+    markdown.render(content).html_safe
+  end
 end
